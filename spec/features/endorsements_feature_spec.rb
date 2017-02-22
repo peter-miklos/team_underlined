631d872b8423272b67af6123cb78a051033dd817
Liam Taylor <liam.taylor888@googlemail.com>
2017-02-22 11:44:30 +0000

    creates failing test for creating endorsements

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -5,4 +5,19 @@ feature 'endorsements' do
     visit '/endorsements'
     expect(page).to have_content('You do not have any endorsements yet')
   end
+
+  scenario 'creating endorsements' do
+    visit '/endorsements'
+    click_button 'Create endorsements'
+    expect(current_path).to eq '/endorsements/new'
+    fill_in "Headline", with: "Great contribution to the project"
+    fill_in "Name", with: "Amanda"
+    fill_in "Description", with: "Really enjoyed working with Amanda"
+    fill_in "Category", with: "teamwork, innovation"
+    click_button "Submit"
+    expect(current_path).to eq "/endorsements"
+    expect(page).not_to have_content('You do not have any endorsements yet')
+    expect(page).to have_content("Endorsements you've received:")
+    expect(page).to have_content("Amanda: Great contribution to project")
+  end
 end
