b29fcf2746f6ed571c7f402e8b5674ebbf3c85ad
Dan PElensky <dan@pelensky.com>
2017-02-22 14:25:43 +0000

    created evidence view

--- spec/features/evidence_feature_spec.rb
+++ spec/features/evidence_feature_spec.rb
@@ -24,3 +24,25 @@ describe "creating evidence" do
     expect(page).to have_content "Attended UCL care..."
   end
 end
+
+describe "viewing evidence" do
+  it "should allow the user to view evidence" do
+    create_evidence
+    expect(page).not_to have_content "No evidence yet."
+    click_link "Attended career fair"
+    expect(page).to have_content "Attended career fair"
+    expect(page).to have_content "Attended UCL career fair to represent HSBC IT. Spoke with a load of grads."
+    expect(page).to have_content "Sponsorship, Role Model"
+    expect(page).to have_content  "2017-01-21"
+  end
+end
+
+def create_evidence
+  visit "/evidences"
+  click_link("Add evidence")
+  fill_in "Headline", with: "Attended career fair"
+  fill_in "Description", with: "Attended UCL career fair to represent HSBC IT. Spoke with a load of grads."
+  fill_in "Category", with: "Sponsorship, Role Model"
+  fill_in "Date achieved", with: "2017-01-21"
+  click_button "Create Evidence"
+end
