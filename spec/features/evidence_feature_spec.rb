14c66941f801f25b6be380c959fbbd1623cab2f5
Peter Miklos <pt.miklos@gmail.com>
2017-02-22 11:52:11 +0000

    first test added to evidence/new

--- spec/features/evidence_feature_spec.rb
+++ spec/features/evidence_feature_spec.rb
@@ -5,5 +5,23 @@ describe "Handling evidence" do
     visit '/evidence'
     expect(page).to have_content "No evidence yet."
   end
+end
 
+describe "creating evidence" do
+  it "should allow the user to add evidence" do
+    visit "/evidence"
+    expect(page).to have_content "No evidence yet."
+    expect(page).to have_link "Add evidence"
+    click_link "Add evidence"
+    expect(page).to have_content "Evidence"
+    fill_in "Headline", with: "Attended career fair"
+    fill_in "Description", with: "Attended UCL career fair to represent HSBC IT. Spoke with a load of grads."
+    fill_in "Category", with: "Sponsorship, Role Model"
+    fill_in "Date Achieved", with: "2017-01-21"
+    click_button "Submit"
+    expect(page).to have_content "Attended career fair"
+    expect(page).to have_content "Attended UCL career fair to represent HSBC IT. Spoke with a load of grads."
+    expect(page).to have_content "Sponsorship, Role Model"
+    expect(page).to have_content "2017-01-21"
+  end
 end
